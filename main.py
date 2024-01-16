c8e0f21d93e8dfe7badcdd1ad62abae68e5f272a
stefanlight <steffmukhin@gmail.com>
2024-01-16 09:56:56 +0300

    Ahh, many changes
    feat: reformat imports
    feat: typing append
    > what i supposed to get here? It's not clear, and neither is the program, which makes it think a bit more. 
    feat: private variables in functions
    > it is mandatory otherwise the program gets confused, because these are functions and there are global variables, which is not good
    BREAKING CHANGE: `json` -> `orjson` to more speed
    > you can don't use, and continue to use json, because there's not big speed difference
    
    I don't guarantee this code'll work and only recommend that you edit your code based on this, because I haven't interacted with tinker and don't fully understand how this works.

--- main.py
+++ main.py
@@ -1,103 +1,127 @@
-import tkinter
-import tkinter.messagebox
-import customtkinter
-import sys
-import serial
-import os
-import json
-from pycaw.pycaw import AudioUtilities
+from os import listdir
+from sys import platform
+from asyncio.tasks import run_coroutine_threadsafe
+from asyncio.events import AbstractEventLoop, get_event_loop, new_event_loop
+
 import ctypes
-import asyncio
+from typing import Any
 
-customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
-customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
-comstate = "normal"
-serialdata = ""
+import orjson
+import serial
+from thinker import messagebox
+from customthinker import (
+    CTk,
+    CTkFrame,
+    CTkLabel,
+    CTkFont,
+    CTkOptionMenu,
+    set_appearance_mode,
+    set_default_color_theme,
+)
+from pycaw.pycaw import AudioUtilities
+
+set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 
-async def change_volume(app: str, newvolume: int):
+class COM:
+    state = "normal"
+
+
+async def change_volume(app: str, new_volume: int):
     if app in await all_apps():
         sessions = AudioUtilities.GetAllSessions()
         for session in sessions:
             volume = session.SimpleAudioVolume
             if session.Process and session.Process.name() == f"{app}.exe":
-                volume.SetMasterVolume(float(newvolume) / 100, None)
+                volume.SetMasterVolume(float(new_volume) / 100, None)
 
 
-async def all_apps():
+async def all_apps() -> list[str]:
     sessions = AudioUtilities.GetAllSessions()
-    applist = []
+    apps: list[str] = []
     for session in sessions:
         try:
             if not session.Process.name() == "LEDKeeper2.exe":
-                applist.append(session.Process.name()[:-4])
-        except:
+                apps.append(session.Process.name()[:-4])
+        except (Exception,):
             pass
-    return applist
+    return apps
 
 
 async def change_mic_volume(volume: int):
-    winmm = ctypes.WinDLL('winmm')
-    winmm.waveOutSetVolume(volume, 0x3333)
+    dll = ctypes.WinDLL('winmm')
+    dll.waveOutSetVolume(volume, 0x3333)
 
 
-async def read_serial(port: str):
-    ser = serial.Serial(port, baudrate=115200, timeout=1)
-    i = 0
-    while i < 2:
-        data = list(ser.readline().decode("ISO-8859-1").strip())
-        i += 1
-    for i in range(len(data)):
-        if data[i] == ",":
-            data[i] = ""
-    res = "".join(data).split()
-    global serialdata
-    serialdata = res
-    await asyncio.sleep(0.1)
+async def read_serial(__port: str) -> list[int]:
+    __serial = serial.Serial(__port, baudrate=115200, timeout=1)
+    __index = 0
+    __data: list[str] | None = None
+    while __index < 2:
+        __data = list(
+            __serial.readline().decode("ISO-8859-1").strip()
+        )
+        __index += 1
+    if __data is None:
+        raise ValueError("Could not read serial port")
+    for __index in range(len(__data)):
+        if __data[__index] == ",":
+            __data[__index] = ""
+    return "".join(__data).split()
 
 
-async def write_serial(port: str, pos1: str, pos2: str, pos3: str):
-    ser = serial.Serial(port, baudrate=115200, timeout=1)
-    ser.write(f"{pos1},{pos2},{pos3}".encode('ascii'))
-    i = 0
-    while i < 2:
-        data = ser.readline().decode("ISO-8859-1").strip()
-        i += 1
-    return data
+async def write_serial(
+        __port: str,
+        pos1: str,
+        pos2: str,
+        pos3: str
+) -> list[int]:
+    __serial = serial.Serial(__port, baudrate=115200, timeout=1)
+    __serial.write(f"{pos1},{pos2},{pos3}".encode('ascii'))
+    __index = 0
+    __data: list[int] | None = None
+    while __index < 2:
+        __data = __serial.readline().decode("ISO-8859-1").strip()
+        __index += 1
+    if not data:
+        raise ValueError("Could not read serial port")
+    return __data
 
 
-async def serial_ports():
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
+async def serial_ports() -> list[str]:
+    if platform.startswith('win'):
+        ports: list[str] = ['COM%s' % (i + 1) for i in range(256)]
     else:
         raise EnvironmentError('Unsupported platform')
-    result = []
-    for port in ports:
-        if port != "COM1":
+    __result: list[str] = []
+    for __port in ports:
+        if __port != "COM1":
             try:
-                s = serial.Serial(port)
-                s.close()
-                result.append(port)
+                __serial = serial.Serial(__port)
+                __serial.close()
+                __result.append(__port)
             except (OSError, serial.SerialException):
                 pass
-    if not result:
-        tkinter.messagebox.showerror(title="No connected devices found",
-                                     message="No connected devices found\nTry to check device connection with PC")
-        global comstate
-        comstate = "disabled"
-    return result
+    if not __result:
+        messagebox.showerror(
+            title="No connected devices found",
+            message="No connected devices found\nTry to check device connection with PC"
+        )
+        COM.state = "disabled"
+    return __result
 
 
-def change_appearance_mode_event(new_appearance_mode: str):
-    customtkinter.set_appearance_mode(new_appearance_mode)
+def change_appearance_mode_event(new_appearance_mode: str) -> None:
+    set_appearance_mode(new_appearance_mode)
 
 
-def change_com_port(port: str):
-    print("New com port: " + port)
+def change_com_port(__port: str) -> None:
+    print("New com port: " + __port)
 
 
-class App(customtkinter.CTk):
-    def __init__(self):
+class App(CTk):
+    def __init__(self) -> None:
         super().__init__()
 
         # configure window
@@ -110,50 +134,62 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
         # create sidebar frame with widgets
-        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame = CTkFrame(self, width=140, corner_radius=0)
         self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(4, weight=1)
-        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="AudioZalupa",
-                                                 font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
-        self.comselector_label = customtkinter.CTkLabel(self.sidebar_frame, text="Select device COM Port:", anchor="w")
-        self.comselector_optionmenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
-                                                                  values=serial_ports(),
-                                                                  command=change_com_port)
-        self.comselector_optionmenu.grid(row=4, column=0, padx=20, pady=(10, 360))
-        self.comselector_label.grid(row=3, column=0, padx=20, pady=(10, 0))
-        self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w")
-        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
-        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame,
-                                                                       values=["Light", "Dark", "System"],
-                                                                       command=change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
 
-        self.appearance_mode_optionemenu.set("Dark")
-        if port != "None":
-            self.comselector_optionmenu.set(port)
-        else:
-            self.comselector_optionmenu.set("Change me")
-        self.comselector_optionmenu.configure(state=comstate)
+        self.logo_label = CTkLabel(
+            self.sidebar_frame,
+            text="AudioZalupa",
+            font=CTkFont(size=20, weight="bold")
+        )
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+
+        self.com_select_label = CTkLabel(
+            self.sidebar_frame,
+            text="Select device COM Port:",
+            anchor="w"
+        )
+        self.com_select_option_menu = CTkOptionMenu(
+            self.sidebar_frame,
+            values=serial_ports(),
+            command=change_com_port
+        )
+        self.com_select_option_menu.grid(row=4, column=0, padx=20, pady=(10, 360))
+        self.com_select_label.grid(row=3, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_label = CTkLabel(
+            self.sidebar_frame, text="Appearance Mode:", anchor="w"
+        )
+        self.appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
+        self.appearance_mode_option_menu = CTkOptionMenu(
+            self.sidebar_frame,
+            values=["Light", "Dark", "System"],
+            command=change_appearance_mode_event
+        )
+        self.appearance_mode_option_menu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.appearance_mode_option_menu.set("Dark")
+
+        self.com_select_option_menu.set("Change me" if port == "None" else port)
+        self.com_select_option_menu.configure(state=COM.state)
 
 
 if __name__ == "__main__":
-    dev = False
-    if not dev:
-        if "config.json" not in os.listdir('./'):
-            with open('config.json', 'w') as f:
-                f.write('{"port":"None"}')
+    loop: AbstractEventLoop = get_event_loop() or new_event_loop()
+    developer_mode: bool = False
+
+    if not developer_mode:
+        if "config.json" not in listdir('./'):
+            with open('config.json', 'w') as stream:
+                stream.write('{"port":"None"}')
         else:
-            with open('config.json', 'r', encoding="utf-8") as f:
-                config_json = json.loads(str(f.read()))
-                port = config_json['port']
-        loop = asyncio.get_event_loop()
-        loop.create_task(read_serial("COM3"))
-        loop.run_forever()
-        print(serialdata)
-        asyncio.run(change_volume("Telegram", serialdata[0]))
+            with open('config.json', 'rb') as stream:
+                data: dict[str, Any] = orjson.loads(stream.read())
+                port: str = data['port']
+
+        serial_data: list[int] = loop.run_until_complete(read_serial("COM3"))
+        run_coroutine_threadsafe(change_volume("Telegram", serial_data[0]), loop)
         app = App()
         app.mainloop()
     else:
         # change_mic_volume(100)
-        exit()
+        exit(0)
